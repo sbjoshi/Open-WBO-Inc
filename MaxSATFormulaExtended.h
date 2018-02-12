66e55bcbd4ee454337a828b7486b63af0176a50e
Prateek Kumar <prateek@prateekkumar.in>
2018-02-12 22:41:20 +0530

    Add new class MaxSATExtended
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

---
+++ MaxSATFormulaExtended.h
@@ -0,0 +1,17 @@
+#ifndef MaxSATFormulaExtended_h
+#define MaxSATFormulaExtended_h
+
+#include "MaxSATFormula.h"
+
+namespace openwbo {
+
+class MaxSATFormulaExtended : public MaxSATFormula {
+
+public:
+  vec<Soft>& getSoftClauses();
+
+}
+
+};
+
+#endif
